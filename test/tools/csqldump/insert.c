b1205e76de62f2adcb594762c6a433cac373d36a
kishoramballi <kishoramballi>
2008-05-09 14:12:18 +0000

    test case 7 from wiki page for csqldump tool

---
+++ test/tools/csqldump/insert.c
@@ -0,0 +1,73 @@
+//TestCase: We need to start new server to test this.
+//the default db size set is not enough to test this
+#include<CSql.h>
+int main()
+{
+    Connection conn;
+    DbRetVal rv = conn.open("root", "manager");
+    if (rv != OK) return 1;
+    DatabaseManager *dbMgr = conn.getDatabaseManager();
+    if (dbMgr == NULL) { printf("Auth failed\n"); return 2;}
+   
+    TableDef tabDef;
+    tabDef.addField("f1", typeInt, 0, NULL, true);
+    tabDef.addField("f2", typeInt);
+    rv = dbMgr->createTable("t1", tabDef);
+    if (rv != OK) { printf("Table creation failed\n"); conn.close(); return 3; }
+    printf("Table created\n");
+    
+HashIndexInitInfo *idxInfo = new HashIndexInitInfo();
+    strcpy(idxInfo->tableName, "t1");
+    idxInfo->list.append("f1");
+    idxInfo->isUnique = true;
+    idxInfo->isPrimary = true;
+    idxInfo->indType = hashIndex;
+    rv = dbMgr->createIndex("indx", idxInfo);
+    if (rv != OK) { printf("Index creation failed\n"); return ErrUnknown; }
+
+    Table *table = dbMgr->openTable("t1");
+    if (table == NULL) 
+    { 
+        printf("Unable to open table\n"); 
+        dbMgr->dropTable("t1");
+        conn.close();
+        return 2; 
+    }
+    int id1 = 0, id2 = 5;
+    table->bindFld("f1", &id1);
+    table->bindFld("f2", &id2);
+    int icount =0;
+    for (int i = 0 ; i < 10000 ; i++)
+    {
+        conn.startTransaction();
+        for (int j = 0 ; j < 100 ; j++) {
+            id1= icount++;
+            rv = table->insertTuple();
+            if (rv != OK) break;
+        }
+        if (rv != OK) break;
+        conn.commit();
+    }
+    printf("Tuples inserted %d\n", icount);
+    table->setCondition(NULL);
+    rv = table->execute();
+    if (rv != OK)
+    {
+       dbMgr->closeTable(table);
+       dbMgr->dropTable("t1");
+       conn.close();
+    }
+    void *fld2ptr, *fld3ptr, *tuple;
+    icount = 0; 
+    while(true)
+    {
+        tuple = (char*)table->fetch() ;
+        if (tuple == NULL) {break;}
+        icount++;
+    }
+    table->close();
+    dbMgr->closeTable(table);
+    conn.close();
+    if (icount != 1000000) return 3;
+    return 0;
+}
---
+++ test/tools/csqldump/test007.ksh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#Run this test only under csql/test or on this directory.
+#Otherwise, it may fail
+executable=${PWD}/tools/csqldump/insert
+REL_PATH=.
+if [ -s "$executable" ]
+then
+    REL_PATH=${PWD}/tools/csqldump
+fi
+
+$REL_PATH/insert >/dev/null 2>&1
+if [ $? -ne 0 ]
+then 
+    exit 1;
+fi
+
+$CSQL_INSTALL_ROOT/bin/csqldump
+if [ $? -ne 0 ]
+then
+    exit 2;
+fi
+
+$CSQL_INSTALL_ROOT/bin/csql -u root -p manager -s $REL_PATH/drop.sql > /dev/null 2>&1
+
+if [ $? -ne 0 ]
+then
+    exit 3;
+fi
+
+exit 0;
