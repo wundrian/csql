bd1adb43a19f818a9d1c3dff33102d9e6b6a0cca
jitendralenka <jitendralenka>
2008-05-28 09:41:54 +0000

    changes in printf function.

--- test/sqlapi/Select/selecttest9.c
+++ test/sqlapi/Select/selecttest9.c
@@ -44,10 +44,12 @@ int main()
 
   strcpy(statement,"DROP TABLE T1;");
   rv = stmt->prepare(statement);
-  if(rv!=OK)
+  if(rv==OK)
   {
      printf("Test script failed\n");
-     return 3;
+     delete stmt;
+     delete con;
+      return 3;
   }
   printf("Test script passed\n");
   stmt->execute(rows);
