6f4bd3ea30dc8c2d9ca37edaffeb07e950e35862
prabatuty <prabatuty>
2008-05-28 19:38:47 +0000

    adding bidirectional cache update progation using triggers in targetdb

--- src/tools/csqlcacheserver.cxx
+++ src/tools/csqlcacheserver.cxx
@@ -15,14 +15,12 @@
   ***************************************************************************/
 #include <AbsSqlConnection.h>
 #include <AbsSqlStatement.h>
-#include <SqlOdbcConnection.h>
-#include <SqlOdbcStatement.h>
 #include <SqlFactory.h>
 #include <CSql.h>
-#include <Network.h>
-#include <SqlLogStatement.h> //for BindSqlField
-#include <SqlNetworkHandler.h>
-#include <SessionImpl.h>
+
+int insert(Table *table, int pkid);
+int remove(Table *table, int pkid);
+int getRecordsFromTargetDb(int mode);
 
 int srvStop =0;
 static void sigTermHandler(int sig)
@@ -37,6 +35,8 @@ void printUsage()
    printf("Description: Start the csql caching server.\n");
    return;
 }
+AbsSqlConnection *targetconn;
+Connection conn;
 int main(int argc, char **argv)
 {
     int c = 0, opt = 0;
@@ -58,79 +58,160 @@ int main(int argc, char **argv)
     os::signal(SIGINT, sigTermHandler);
     os::signal(SIGTERM, sigTermHandler);
 
-    SessionImpl *impl = new SessionImpl();
-    impl->readConfigFile();
-    bool end = false;
-    SqlNetworkHandler::type = CSqlAdapter;
-    SqlNetworkHandler::conn = SqlFactory::createConnection(CSqlAdapter);
-    DbRetVal rv = SqlNetworkHandler::conn->connect("root", "manager");
+    targetconn = SqlFactory::createConnection(CSqlAdapter);
+    DbRetVal rv = targetconn->connect("root", "manager");
+    if (rv != OK) return 1;
+    rv = conn.open("root", "manager");
     if (rv != OK) return 1;
     if (!Conf::config.useCache())
     {
         printf("Cache is set to OFF in csql.conf file\n");
-        SqlNetworkHandler::conn->disconnect();
         return 1;
     } 
-     
-    
-    FILE *fp = NULL;
-    int nwid =0;
-    char hostname[IDENTIFIER_LENGTH];
-    char nwmode ='a';
-    int port=0;
-    fp = fopen(Conf::config.getReplConfigFile(),"r");
-    if( fp == NULL ) {
-        printError(ErrSysInit, "Invalid path/filename for NETWORK_CONFIG_FILE.\n");
-        SqlNetworkHandler::conn->disconnect();
-        return 1;
-    }
-    bool found =false;
-    while(!feof(fp)) {
-       fscanf(fp, "%d:%d:%s\n", &nwid, &port, hostname);
-       printf( "%d:%d:%s\n", nwid, port, hostname);
-       if (nwid == Conf::config.getNetworkID()) { found = true; break;}
-    }
-    fclose(fp);
-    if (!found)
-    {
-        printf("Info not found in NETWORK_CONFIG_FILE for nwid %d\n", 
-               Conf::config.getNetworkID());
-        SqlNetworkHandler::conn->disconnect();
-        return 1;
-    }
-    NetworkServer *nwServer;
 
-    nwServer = new TCPServer();
-    
-    nwServer->setServerPort(port);
-    printf("Starting the cache server\n");
-    rv = nwServer->start();
-    if (rv != OK) {
-        printf("Unable to start the server\n");
-        return 1;
-    }
     printf("Cache server started\n");
-    fd_set fdset;
     int ret = 0;
     struct timeval timeout, tval;
-    timeout.tv_sec = 5;
+    timeout.tv_sec = 30; //TODO::should be an csql.conf parameter
     timeout.tv_usec = 0;
     
     while(!srvStop)
     {
-        FD_ZERO(&fdset);
-        FD_SET(nwServer->getSocket(), &fdset);
         tval.tv_sec = timeout.tv_sec;
         tval.tv_usec = timeout.tv_usec;
-        ret = os::select(nwServer->getSocket()+1, &fdset, 0, 0, &tval);
-        if (ret > 0) {
-            nwServer->handleClient();
-        }
-        printf("Server Waiting for clients\n");
+        ret = os::select(0, NULL, 0, 0, &tval);
+        printf("Getting the updates\n");
+        ret = getRecordsFromTargetDb(1);       
+        printf("Return value is %d\n", ret);
+        if (ret !=0) srvStop = 1;
+        //ret = getRecordsFromTargetDb(2);       
+        if (ret !=0) srvStop = 1;
     }
     printf("Cache Server Exiting\n");
-    nwServer->stop();
-    SqlNetworkHandler::conn->disconnect();
-    delete SqlNetworkHandler::conn;
+    conn.close();
+    targetconn->disconnect();
     return 0;
 }
+int getRecordsFromTargetDb(int mode)
+{
+    int pkid;
+    char tablename[64];
+    int op, id;
+    int rows =0;
+    DbRetVal rv = OK;
+    AbsSqlStatement *stmt = SqlFactory::createStatement(CSqlAdapter);
+    stmt->setConnection(targetconn);
+    AbsSqlStatement *delstmt = SqlFactory::createStatement(CSqlAdapter);
+    delstmt->setConnection(targetconn);
+    if (mode == 1 ) {
+        rv = stmt->prepare("SELECT * FROM csql_log_int;");
+        rv = delstmt->prepare("DELETE FROM csql_log_int where id =?;");
+    }
+    else {
+        rv = stmt->prepare("SELECT * FROM csql_log_char;");
+        rv = delstmt->prepare("DELETE FROM csql_log_char where id =?;");
+    }
+    if (rv != OK) return 1;
+    stmt->bindField(1, tablename);
+    stmt->bindField(2, &pkid);
+    stmt->bindField(3, &op);
+    stmt->bindField(4, &id);
+    DatabaseManager *dbMgr = conn.getDatabaseManager();
+    while(true) {
+      rv = targetconn->beginTrans();
+      rv = stmt->execute(rows);
+      if (rv != OK) 
+      {
+          printError(ErrSysInit, "Unable to execute stmt in target db");
+          return 1;
+      }
+      if (stmt->fetch() != NULL) {
+          printf("Row value is %s %d %d\n", tablename, pkid, op);
+          Table *table = dbMgr->openTable(tablename);
+          int ret = 0;
+          if (table == NULL)
+          {
+              printError(ErrSysInit, "Table %s not exist in csql", tablename);
+              break;
+          }
+          if (op == 3) //DELETE
+          {
+              ret = remove(table, pkid);
+          }else if (op == 2)// UPDATE
+          {
+              ret = remove(table,pkid);
+              ret = insert(table, pkid);
+          }
+          else //INSERT
+          {
+              ret = insert(table, pkid);
+          }
+          dbMgr->closeTable(table);
+          rv = targetconn->commit();
+          rv = targetconn->beginTrans();
+          //Remove record from csql_log_XXX table
+          delstmt->setIntParam(1, id);
+          delstmt->execute(rows);
+          if (rows != 1) printf("log record not deleted from the target db\n");
+          
+          rv = targetconn->commit();
+          //create table csql_log_int(tablename char(64), pkid int, op int, id int not null unique auto_increment);
+          //insert ino csql_log_int(tablename, pkid, op) values ('t1', 100, 1);
+     }
+     stmt->close();
+   }
+   return 0;
+}
+int insert(Table *table, int pkid)
+{
+    AbsSqlStatement *stmt = SqlFactory::createStatement(CSqlAdapter);
+    stmt->setConnection(targetconn);
+    char sbuf[1024];
+    sprintf(sbuf, "SELECT * FROM %s where f1 = %d;", table->getName(), pkid);
+    //TODO::get the primary key field name from the table interface. need to implement it
+    DbRetVal rv = stmt->prepare(sbuf);
+    if (rv != OK) return 1;
+
+    List fNameList = table->getFieldNameList();
+    ListIterator fNameIter = fNameList.getIterator();
+    FieldInfo *info = new FieldInfo();
+    int fcount =1; void *valBuf; int fieldsize=0;
+    Identifier *elem = NULL;
+    while (fNameIter.hasElement()) {
+        elem = (Identifier*) fNameIter.nextElement();
+        table->getFieldInfo((const char*)elem->name, info);
+        valBuf = AllDataType::alloc(info->type, info->length);
+        table->bindFld(elem->name, valBuf);
+        stmt->bindField(fcount++, valBuf);
+    }
+    delete info;
+    int rows=0;
+    int retValue = stmt->execute(rows);
+    if (retValue && rows != 1) {printError(ErrSysInit, "Unable to execute statement at target db\n"); return ErrSysInit; }
+    conn.startTransaction();
+    if (stmt->fetch() != NULL) {
+        table->insertTuple();
+    }
+    conn.commit();      
+    return 0;
+}
+int remove(Table *table, int pkid)
+{
+    DbRetVal rv = OK;
+    Condition p1;
+    //TODO::get the primary key field name from the table interface. need to implement it
+    p1.setTerm("f1", OpEquals, &pkid);
+    table->setCondition(&p1);
+    rv = conn.startTransaction();
+    if (rv != OK) return 1;
+    rv = table->execute();
+    if (rv != OK)  { conn.rollback(); return 1;}
+    if (table->fetch() != NULL)
+        rv = table->deleteTuple();
+    if (rv != OK)  { conn.rollback(); return 1;}
+    table->setCondition(NULL);
+    rv = conn.commit();
+    if (rv != OK) return 1;
+    return 0;
+}
+
