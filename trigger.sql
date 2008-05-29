b96c747d8b68e7ee5aa1533e5da78a21116efbe5
prabatuty <prabatuty>
2008-05-29 11:17:35 +0000

    *** empty log message ***

---
+++ trigger.sql
@@ -0,0 +1,28 @@
+use test;
+drop trigger if exists triggerinsertp1;
+drop trigger if exists triggerupdatep1;
+drop trigger if exists triggerdeletep1;
+
+DELIMITER | 
+create trigger triggerinsertp1
+AFTER INSERT on p1
+FOR EACH ROW
+BEGIN
+Insert into csql_log_int (tablename, pkid, operation) values ('p1', NEW.f1, 1);
+End; 
+
+create trigger triggerupdatep1
+AFTER UPDATE on p1
+FOR EACH ROW
+BEGIN
+Insert into csql_log_int (tablename, pkid, operation) values ('p1', OLD.f1, 2);
+Insert into csql_log_int (tablename, pkid, operation) values ('p1', NEW.f1, 1);
+End; 
+
+create trigger triggerdeletep1
+AFTER DELETE on p1
+FOR EACH ROW
+BEGIN
+Insert into csql_log_int (tablename, pkid, operation) values ('p1', OLD.f1, 2);
+End; 
+|
