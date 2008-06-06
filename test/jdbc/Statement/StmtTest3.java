a78700fa06c422be575c25fc1effdb8cde51b441
xieliang <xieliang>
2008-06-06 02:55:41 +0000

    repair test script

--- test/jdbc/Statement/StmtTest3.java
+++ test/jdbc/Statement/StmtTest3.java
@@ -14,47 +14,50 @@ public class StmtTest3
            con.commit();
            con.close();
 
-		   //after close the connection,execute "INSERT" statement,it should fail
-		   try
-		   {
+           //after close the connection,execute "INSERT" statement,it should fail
+           try
+           {
                cStmt.execute("INSERT INTO T1 VALUES (1, 'FIRST');");
-			   System.exit(1);
-		   }
-		   catch(Exception e)
-		   {
-		       System.out.println("insert exception---" + e.getMessage());
-		   }
+               System.exit(1);
+           }
+           catch(Exception e)
+           {
+               System.out.println("insert exception---" + e.getMessage());
+           }
 
            //after close the connection,execute "QUERY" statement,it should fail
-		   try
-		   {
+           try
+           {
                cStmt.executeQuery("SELECT * from T1 where f1 = 1;");
-			   System.exit(2);
-		   }
-		   catch(Exception e)
-		   {
-		       System.out.println("query exception---" + e.getMessage());
-		   }
+               System.exit(2);
+           }
+           catch(Exception e)
+           {
+               System.out.println("query exception---" + e.getMessage());
+           }
 
-		   //after close the connection,execute "UPDATE" statement,it should fail
-		   try
-		   {
+           //after close the connection,execute "UPDATE" statement,it should fail
+           try
+           {
                cStmt.executeUpdate("UPDATE T1 SET f2 = 'CHANGED' WHERE f1 = 1;");
-			   System.exit(3);
-		   }
-		   catch(Exception e)
-		   {
-		       System.out.println("update exception---" + e.getMessage()); 
-		   }
+               System.exit(3);
+           }
+           catch(Exception e)
+           {
+               System.out.println("update exception---" + e.getMessage()); 
+           }
 
            //cleanup 
-           cStmt.execute("DROP TABLE T1 ;");
-		   cStmt.close();
-           con.close();
+           cStmt.close();
+           Connection con1 = DriverManager.getConnection("jdbc:csql", "root", "manager");
+           Statement cStmt1 = con1.createStatement();
+           cStmt1.execute("DROP TABLE T1 ;");
+           cStmt1.close();
+           con1.close();
         }catch(Exception e) {
             System.out.println("Exception in Test: " + e.getMessage());
             e.printStackTrace();
-			System.exit(4);
+            System.exit(4);
         }
     }
 }
