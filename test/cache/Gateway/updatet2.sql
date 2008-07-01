b5a9e043971589fc4a2737b4c80065b48afef8d5
kishoramballi <kishoramballi>
2008-07-01 07:14:33 +0000

    input sql files for the test scripts

---
+++ test/cache/Gateway/updatet2.sql
@@ -0,0 +1,7 @@
+update t2 set t2f2=1995, t2f9 = '1971/12/25 11:42:35' where t2f1 = 2;
+update t2 set t2f3=333666999, t2f9 = '1973/08/15 13:45:36' where t2f1 = 3;
+update t2 set t2f4='poonam', t2f9 = '1974/12/25 14:42:35' where t2f1 = 4;
+update t2 set t2f7='1978/09/12', t2f9 = '1977/12/25 17:42:35' where t2f1 = 7;
+update t2 set t2f8='12:34:56', t2f9 = '1978/12/25 18:42:35' where t2f1 =8;
+update t2 set t2f9='1978/09/12 12:34:56', t2f9 = '1979/12/25 19:42:35' where t2f1 = 9; 
+
