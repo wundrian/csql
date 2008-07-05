d60ad20e579a4ce3e61e0503241ef2db160885c9
prabatuty <prabatuty>
2008-07-05 07:44:34 +0000

    help files for postgres configuration

---
+++ cachetable.sql
@@ -0,0 +1,21 @@
+#MySQL
+Command
+-------
+$mysql -u root -p (or)
+$mysql -u root -p <filename
+
+
+Statements
+----------
+create table csql_log_int(tablename char(64), pkid int, operation int, id int not null unique auto_increment)engine='innodb';
+
+#POSTGRES
+Command
+-------
+$psql test (or)
+$psql test -f filename
+
+Statements
+----------
+create table csql_log_int(tablename varchar(64), pkid int, operation int);
+alter table csql_log_int add id serial;
