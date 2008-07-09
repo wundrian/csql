918602bf74949084a5f6fc73a2ee446d984de83f
kishoramballi <kishoramballi>
2008-07-09 12:33:28 +0000

    composite keys are supported now.

--- test/dbapi/Index/compositekeyindex1.c
+++ test/dbapi/Index/compositekeyindex1.c
@@ -26,7 +26,7 @@ int main()
     idxInfo->indType = hashIndex;
     rv = dbMgr->createIndex("indx1", idxInfo);
     int ret = 0;
-    if (rv == OK) ret =1;
+    if (rv != OK) ret =1;
     delete idxInfo;
     dbMgr->dropTable("t1");
     conn.close();
