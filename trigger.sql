e9b5113a6d031ba7cace9a155b22f0bee2947498
bijaya <bijaya>
2008-09-10 10:56:28 +0000

    1981115 Mutiple bidirectional cache nodes for single data source

--- trigger.sql
+++ trigger.sql
@@ -8,21 +8,21 @@ create trigger triggerinsertp1
 AFTER INSERT on p1
 FOR EACH ROW
 BEGIN
-Insert into csql_log_int (tablename, pkid, operation,cacheid) values ('p1', NEW.r, 1,1);
+Insert into csql_log_int (tablename, pkid, operation,cacheid) values ('p1', NEW.f1, 1,1);
 End; 
 
 create trigger triggerupdatep1
 AFTER UPDATE on p1
 FOR EACH ROW
 BEGIN
-Insert into csql_log_int (tablename, pkid, operation,cacheid) values ('p1', OLD.r, 2,1);
-Insert into csql_log_int (tablename, pkid, operation,cacheid) values ('p1', NEW.r, 1,1);
+Insert into csql_log_int (tablename, pkid, operation,cacheid) values ('p1', OLD.f1, 2,1);
+Insert into csql_log_int (tablename, pkid, operation,cacheid) values ('p1', NEW.f1, 1,1);
 End; 
 
 create trigger triggerdeletep1
 AFTER DELETE on p1
 FOR EACH ROW
 BEGIN
-Insert into csql_log_int (tablename, pkid, operation,cacheid) values ('p1', OLD.r, 2,1);
+Insert into csql_log_int (tablename, pkid, operation,cacheid) values ('p1', OLD.f1, 2,1);
 End; 
 |
