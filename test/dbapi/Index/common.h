159a6919d71d4bef465603a1166edc63de57e306
niharlcp1 <niharlcp1>
2008-10-11 09:17:37 +0000

    Test Script for index on different field

--- test/dbapi/Index/common.h
+++ test/dbapi/Index/common.h
@@ -55,3 +55,68 @@ int insertTupleWithSameValue(DatabaseManager *dbMgr, Connection &conn)
     dbMgr->closeTable(table);
     return icount;
 }
+
+
+int insertTuplef2NUL(DatabaseManager *dbMgr, Connection &conn)
+{
+    Table *table = dbMgr->openTable("t1");
+    if (table == NULL)
+    {
+        printf("Unable to open table\n");
+        return 0;
+    }
+    int id1 = 0, id2;
+    int count=0;
+    table->bindFld("f1", &id1);
+    //table->bindFld("f2", &id2);
+    DbRetVal rv = OK;
+    for( int i=10;i<=20;i=i+10)
+    {
+        conn.startTransaction();
+        id1= i;
+        rv = table->insertTuple();
+        if (rv != OK) { printf("Insertion failed "); break; }
+        count++;
+        conn.commit();
+    }
+    printf("Tuple inserted is %d \n",count);
+    dbMgr->closeTable(table);
+    return 0;
+}
+
+int creatTable(DatabaseManager *dbMgr)
+{
+    TableDef tabDef;
+    tabDef.addField("f1", typeInt, 0, NULL, true);
+    tabDef.addField("f2", typeInt);
+    DbRetVal rv = dbMgr->createTable("t1", tabDef);
+    if (rv != OK) { printf("Table creation failed\n"); return 1; }
+    printf("Table created\n");
+    return 0;
+}
+int createIndex(DatabaseManager *dbMgr)
+{
+    //Creating hash index on field f1 of table t1  
+    HashIndexInitInfo *idxInfo = new HashIndexInitInfo();
+    strcpy(idxInfo->tableName, "t1");
+    idxInfo->list.append("f1");
+    idxInfo->indType = hashIndex;
+
+    DbRetVal rv = dbMgr->createIndex("indx1", idxInfo);
+    if (rv != OK) { printf("Index creation failed\n"); return 1; }
+    printf("Index created for t1(f1)\n");
+
+    //Creating hash index on field f2 of table t1 
+    strcpy(idxInfo->tableName, "t1");
+    idxInfo->list.remove("f1");
+    idxInfo->list.append("f2");
+    idxInfo->indType = hashIndex;
+
+    rv = dbMgr->createIndex("indx2", idxInfo);
+    if (rv != OK) { printf("Index creation failed\n"); return 1; }
+    printf("Index created for t1(f2)\n");
+
+    delete idxInfo;
+    return 0;
+}
+             
---
+++ test/dbapi/Index/index1.c
@@ -0,0 +1,25 @@
+//create table t1(f1 int,f2 int)
+//create index idx1 on t1(f1)
+//create index idx2 on t1(f2)
+//insert null records for f2 and check whether the system is stable after that.
+
+#include<CSql.h>
+#include"common.h"
+int main()
+{
+    Connection conn;
+    DbRetVal rv=conn.open("root","manager");
+    if(rv != OK) return 1;
+    DatabaseManager *dbmgr=conn.getDatabaseManager();
+    if(dbmgr == NULL) { printf("Authentication Failed\n"); return 2; }
+    
+    if( creatTable(dbmgr) !=0 ) { conn.close(); return 3; }
+    if( createIndex(dbmgr) != 0 ) { dbmgr->dropTable("t1"); conn.close(); return 4; }
+    int flag = insertTuplef2NUL(dbmgr,conn);
+    if( flag != 0 ) { dbmgr->dropTable("t1"); conn.close(); return 5; }
+    dbmgr->dropTable("t1");
+    conn.close();
+    return 0;
+}
+
+
