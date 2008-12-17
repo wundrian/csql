ce787f4d65055c16b31b71dbfec64a2d0b10aed6
niharlcp1 <niharlcp1>
2008-12-17 14:54:26 +0000

    Testing BETWEEN operator in JOIN

---
+++ test/sql/Join/join_between.sql
@@ -0,0 +1,10 @@
+echo select * from emp,dept;
+select * from emp,dept;
+echo select * from emp,dept where emp.dno between 10 and 30;
+select * from emp,dept where emp.dno between 10 and 30;
+echo select * from emp,dept where dept.deptno between 20 and 40;
+select * from emp,dept where dept.deptno between 20 and 40;
+echo select * from emp,dept where emp.dno between 15 and 30  and dept.deptno between 20 and 40;
+select * from emp,dept where emp.dno between 15 and 30  and dept.deptno between 20 and 40;
+echo select * from emp,dept where emp.dno between 15 and 30 or dept.deptno between 20 and 40;
+select * from emp,dept where emp.dno between 15 and 30 or dept.deptno between 20 and 40;
