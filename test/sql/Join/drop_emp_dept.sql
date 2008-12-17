a22ecd6124ebdd729afb2e5a845c2429f0f39db2
niharlcp1 <niharlcp1>
2008-12-17 13:41:51 +0000

    Testing IN operator in JOIN

---
+++ test/sql/Join/drop_emp_dept.sql
@@ -0,0 +1,4 @@
+echo drop table emp;
+drop table emp;
+echo drop table dept;
+drop table dept;
---
+++ test/sql/Join/join_in.sql
@@ -0,0 +1,10 @@
+echo select * from emp,dept;
+select * from emp,dept;
+echo select * from emp,dept where emp.dno in(20,30);
+select * from emp,dept where emp.dno in(20,30);
+echo select * from emp,dept where dept.deptno in(20,30);
+select * from emp,dept where dept.deptno in(20,30);
+echo select * from emp,dept where emp.dno in(20,30)  and dept.deptno in(20,30);
+select * from emp,dept where emp.dno in(20,30)  and dept.deptno in(20,30);
+echo select * from emp,dept where emp.dno in(20,30) or dept.deptno in(20,30);
+select * from emp,dept where emp.dno in(20,30) or dept.deptno in(20,30);
