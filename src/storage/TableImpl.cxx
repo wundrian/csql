791c5d8ea0e67bbaff4a36805fdee9ddf79e9117
bijaya <bijaya>
2009-01-03 05:31:13 +0000

    bug id: 2483304 Lock is not released taken by delete tuple

--- src/storage/TableImpl.cxx
+++ src/storage/TableImpl.cxx
@@ -500,6 +500,8 @@ DbRetVal TableImpl::deleteTuple()
             return ret;
         }
     }
+    lMgr_->releaseLock(curTuple_);
+    (*trans)->removeFromHasList(db_, curTuple_);
     ((Chunk*)chunkPtr_)->free(db_, curTuple_);
     if (undoFlag)
         ret = (*trans)->appendUndoLog(sysDB_, DeleteOperation, curTuple_, length_);
