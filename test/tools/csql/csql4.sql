d5e32c46be34fc561f01df527a480865925c270c
niharlcp1 <niharlcp1>
2009-01-06 09:01:16 +0000

    now 14 data types are used in this script

--- test/tools/csql/csql4.sql
+++ test/tools/csql/csql4.sql
@@ -1,5 +1,7 @@
 echo all data type create, insert, select test;
-create table t1 (f1 int, f2 smallint, f3 char(10),f4 float, f5 double,f6 date, f7 time, f8 timestamp,f9 tinyint,f10 bigint,f11 binary(128),f12 integer,f13 real);
-insert into t1 values(1, 1,'NIHAR', 1.1,10000001.10000001, '2001-1-1', '01:01:01', '2001-11-30 01:01:01',1,10000000,'ABCDEF0123456789',2147483647,-123.456);
+echo create table t1 (f1 char(10), f2 tinyint, f3 smallint,f4 int, f5 bigint,f6 integer, f7 float, f8 real,f9 double,f10 long,f11 binary(128),f12 date,f13 time,f14 timestamp);
+create table t1 (f1 char(10), f2 tinyint, f3 smallint,f4 int, f5 bigint,f6 integer, f7 float, f8 real,f9 double,f10 long,f11 binary(128),f12 date,f13 time,f14 timestamp);
+echo insert into t1 values('NIHAR', 1,1,-2147483648,9223372036854775807, 2147483648,1.1, 1000000000.000000,10000001.10000001,99999,'ABCDEF0123456789','2001-1-1', '01:01:01', '2001-11-30 01:01:01');
+insert into t1 values('NIHAR', 1,1,-2147483648,9223372036854775807, 2147483648,1.1, 1000000000.000000,10000001.10000001,99999,'ABCDEF0123456789','2001-1-1', '01:01:01', '2001-11-30 01:01:01');
 select * from t1;
 drop table t1;
