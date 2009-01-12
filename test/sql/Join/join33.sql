5428e8ffc19853700adba6486e6320372c37762b
jitendralenka <jitendralenka>
2009-01-12 16:53:44 +0000

    Modified existinf files

---
+++ test/sql/Join/join33.sql
@@ -0,0 +1,11 @@
+create table emp(eno int,ename char(10),dno int);
+insert into emp values(1,'Nihar',10);
+insert into emp values(2,'Kishore',20);
+insert into emp values(3,'Jiten',30);
+insert into emp values(4,'Bijay',40);
+insert into emp values(5,'Kailash',50);
+create table dept(deptno int,dname char(10));
+insert into dept values(20,'Sales');
+insert into dept values(40,'Mkt');
+insert into dept values(35,'HR');
+
