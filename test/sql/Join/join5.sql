d280eeb9f9e610b6f28d7a22a04d8de03d391fcf
niharlcp1 <niharlcp1>
2009-01-15 13:27:31 +0000

    Join

--- test/sql/Join/join5.sql
+++ test/sql/Join/join5.sql
@@ -1,3 +1,4 @@
 echo select t1.f1,t2.f1,t3.f1 from t1,t2,t3 where t1.f2=t2.f2 or t1.f1 > 100;
 select t1.f1,t2.f1,t3.f1 from t1,t2,t3 where t1.f2=t2.f2 or t1.f1 > 100;
-
+echo select t1.f1, t2.f1, t3.f1 from t1,t2,t3 where t1.f2= t2.f2 and t1.f3 = t3.f2;
+select t1.f1, t2.f1, t3.f1 from t1,t2,t3 where t1.f2= t2.f2 and t1.f3 = t3.f2;
