b62b7db3ebe6c857e4ac6528451463edd3dd4607
bijaya <bijaya>
2009-10-08 09:58:56 +0000

    Table T1 changes to t1

--- demo/gateway/jdbc/GwStmt.java
+++ demo/gateway/jdbc/GwStmt.java
@@ -1,4 +1,4 @@
-//1. Table T1 is cached to csql. 
+//1. Table t1 is cached to csql. 
 //Then do (INSERT/UPDATE/DELETE/SELECT) with params statement using gateway statement
 
 import java.sql.Connection;
@@ -21,7 +21,7 @@ public class GwStmt {
            Connection con = DriverManager.getConnection("jdbc:gateway", "root", "manager");
            Statement cStmt = con.createStatement();
            PreparedStatement stmt = null, selStmt= null;
-           stmt = con.prepareStatement("INSERT INTO T1 VALUES (?, ?);");
+           stmt = con.prepareStatement("INSERT INTO t1 VALUES (?, ?);");
            for (int i =0 ; i< 10 ; i++) {
              stmt.setInt(1, i);
              stmt.setString(2, String.valueOf(i+100));
@@ -29,7 +29,7 @@ public class GwStmt {
            }
            stmt.close();
            con.commit();
-           selStmt = con.prepareStatement("SELECT * from T1 where f1 = ?;");
+           selStmt = con.prepareStatement("SELECT * from t1 where f1 = ?;");
            ResultSet rs = null;
            for (int i =0 ; i< 10 ; i++) {
              selStmt.setInt(1, i);
@@ -41,7 +41,7 @@ public class GwStmt {
              rs.close();
            }
            int ret=0;
-           stmt = con.prepareStatement("UPDATE T1 SET f2 = ? WHERE f1 = ?;");
+           stmt = con.prepareStatement("UPDATE t1 SET f2 = ? WHERE f1 = ?;");
            for (int i =0 ; i< 10 ; i +=2) {
              stmt.setString(1, String.valueOf(i+200));
              stmt.setInt(2, i);
@@ -61,7 +61,7 @@ public class GwStmt {
              rs.close();
            }
            con.commit();
-           stmt = con.prepareStatement("DELETE FROM T1 WHERE f1 = ?;");
+           stmt = con.prepareStatement("DELETE FROM t1 WHERE f1 = ?;");
            for (int i =0 ; i< 10 ; i +=3) {
              stmt.setInt(1, i);
              ret = stmt.executeUpdate();
