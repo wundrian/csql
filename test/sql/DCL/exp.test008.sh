ee01429a6c7581fecb3416e7f57926803302a748
Matti Bickel <matti.bickel@rateu.de>
2012-05-11 00:05:02 +0200

    test: No GRANT SELECT on any table in a join causes statement failure (re #9)

---
+++ test/sql/DCL/exp.test008.sh
@@ -0,0 +1,14 @@
+Statement Executed
+Statement Executed
+Statement Executed
+Statement Executed: Rows Affected = 0
+Statement Executed: Rows Affected = 1
+Statement Executed: Rows Affected = 1
+Statement Executed: Rows Affected = 1
+Statement Executed: Rows Affected = 1
+Statement Executed: Rows Affected = 1
+Statement prepare failed with error -3
+Statement Executed
+Statement Executed
+Statement Executed
+Test Passed
---
+++ test/sql/DCL/test008.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#Test Case : Need SELECT privileges on all tables involved in join
+
+MY_PATH="$(dirname $0)"
+
+TEST_USER="mbickel"
+TEST_PW="test"
+
+exitStatus()
+{
+    rm -f "${MY_PATH}/"*.sql
+    echo "Test $1"
+    exit $2;  
+}
+
+VALGRIND_RUN="valgrind --tool=memcheck --leak-check=full"
+[ "$VALGRIND" = "true" ] || VALGRIND_RUN=""
+
+echo "
+create user ${TEST_USER} password '${TEST_PW}';
+create table s (id integer);
+create table t (id integer);
+grant select on t to ${TEST_USER} where id = 1;
+insert into t values (1);
+insert into t values (2);
+insert into s values (1);
+insert into s values (2);
+insert into s values (3);
+quit;" > "${MY_PATH}/as_root.sql"
+
+echo "
+select * from t, s;
+quit;" > "${MY_PATH}/as_usr.sql"
+
+echo "
+drop table t;
+drop table s;
+drop user ${TEST_USER};
+quit;" > "${MY_PATH}/cleanup.sql"
+
+${VALGRIND_RUN} ${CSQL_INSTALL_ROOT}/bin/csql -u root -p manager -s "${MY_PATH}/as_root.sql"
+[[ $? -ne 0 ]] && exitStatus "Failed" 1
+
+${VALGRIND_RUN} ${CSQL_INSTALL_ROOT}/bin/csql -u "${TEST_USER}" -p "${TEST_PW}" -s "${MY_PATH}/as_usr.sql"
+[[ $? -ne 0 ]] && exitStatus "Failed" 2
+
+${VALGRIND_RUN} ${CSQL_INSTALL_ROOT}/bin/csql -u root -p manager -s "${MY_PATH}/cleanup.sql"
+[[ $? -ne 0 ]] && exitStatus "Failed" 3
+
+exitStatus Passed 0;
