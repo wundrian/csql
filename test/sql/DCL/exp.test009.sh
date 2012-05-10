5558bed2f62517fd6013755690f23bdf87f9e262
Matti Bickel <matti.bickel@rateu.de>
2012-05-11 00:06:15 +0200

    test: cross join respects row level grant predicate on one table (re #13, #9)

---
+++ test/sql/DCL/exp.test009.sh
@@ -0,0 +1,24 @@
+Statement Executed
+Statement Executed
+Statement Executed
+Statement Executed: Rows Affected = 0
+Statement Executed: Rows Affected = 0
+Statement Executed: Rows Affected = 1
+Statement Executed: Rows Affected = 1
+Statement Executed: Rows Affected = 1
+Statement Executed: Rows Affected = 1
+Statement Executed: Rows Affected = 1
+---------------------------------------------------------
+	t.id	s.id	
+---------------------------------------------------------
+	1	1	
+	1	2	
+	1	3	
+	2	1	
+	2	2	
+	2	3	
+
+Statement Executed
+Statement Executed
+Statement Executed
+Test Passed
